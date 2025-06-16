2e6440dc9306ccaeb883d36538457edbb293c8d5
Alister Burt <alisterburt@gmail.com>
2025-06-16 14:26:22 -0700

    ensure that Path1D object works correctly when on CUDA GPUs (#6)

--- tests/test_path_1d.py
+++ tests/test_path_1d.py
@@ -1,30 +1,39 @@
 import numpy as np
+import pytest
 import torch
 
 from torch_subtract_membranes_2d.path_models.path_1d import Path1D
 
+# parameter for cuda device tests which skips test if cuda not available
+cuda_device_parameter = pytest.param("cuda", marks=pytest.mark.skipif(
+    not torch.cuda.is_available(),
+    reason="CUDA not available"
+))
 
-def test_open_path_interpolation():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_open_path_interpolation(device):
     # simple line from [0, 0] to [3, 0]
     control_points = np.array([0, 1, 2, 3])
+    control_points = torch.tensor(control_points, device=device, dtype=torch.float32)
     path = Path1D(control_points=control_points, is_closed=False)
 
     u0 = torch.tensor([0]).float()
     interp_u0 = path.interpolate(u0)
-    assert torch.allclose(interp_u0, torch.tensor([0.0, ]), atol=1e-5)
+    assert torch.allclose(interp_u0, torch.tensor([0.0, ], device=device), atol=1e-5)
 
     u0_5 = torch.tensor([0.5]).float()
     interp_u0_5 = path.interpolate(u0_5)
-    assert torch.allclose(interp_u0_5, torch.tensor([1.5, ]), atol=1e-5)
+    assert torch.allclose(interp_u0_5, torch.tensor([1.5, ], device=device), atol=1e-5)
 
     u1 = torch.tensor([1]).float()
     interp_u1 = path.interpolate(u1)
-    assert torch.allclose(interp_u1, torch.tensor([3.0, ]), atol=1e-5)
+    assert torch.allclose(interp_u1, torch.tensor([3.0, ], device=device), atol=1e-5)
 
-
-def test_closed_path_interpolation():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_closed_path_interpolation(device):
     # closed CW path with xy coordinates
     control_points = np.array([0, 1, 2, 3, 2, 1])
+    control_points = torch.tensor(control_points, device=device, dtype=torch.float32)
     path = Path1D(control_points=control_points, is_closed=True)
 
     # start point should be the same as end point
@@ -33,17 +42,17 @@ def test_closed_path_interpolation():
     interp_u0 = path.interpolate(u0)
     interp_u1 = path.interpolate(u1)
     assert torch.allclose(interp_u0, interp_u1, atol=1e-5)
-    assert torch.allclose(interp_u0, torch.tensor([0.]), atol=1e-5)
+    assert torch.allclose(interp_u0, torch.tensor([0.], device=device), atol=1e-5)
 
     # should be 3 in middle...
     u0_5 = torch.tensor([0.5]).float()
     interp_u0_5 = path.interpolate(u0_5)
-    assert torch.allclose(interp_u0_5, torch.tensor([3.0, ]), atol=1e-5)
+    assert torch.allclose(interp_u0_5, torch.tensor([3.0, ], device=device), atol=1e-5)
 
-
-def test_optimization():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_optimization(device):
     # setup some initial control points
-    initial_control_points = torch.zeros(size=(30, ))
+    initial_control_points = torch.zeros(size=(30, ), device=device)
     path = Path1D(control_points=initial_control_points, is_closed=False)
     path.control_points.requires_grad_(True)
 
@@ -60,7 +69,7 @@ def test_optimization():
         optimizer.zero_grad()
 
         # random samples
-        u = torch.rand(size=(100,))
+        u = torch.rand(size=(100,), device=device)
         ground_truth_x = u
         ground_truth_y = f(ground_truth_x)
 
