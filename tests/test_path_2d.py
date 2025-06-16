a6e573a3c73b63c81caddc0b79bfee40e5dcefc5
Alister Burt <burt.alister@gene.com>
2025-06-16 14:10:43 -0700

    ensure that Path2D object works correctly when on CUDA GPUs

--- tests/test_path_2d.py
+++ tests/test_path_2d.py
@@ -5,6 +5,12 @@ import torch
 
 from torch_subtract_membranes_2d.path_models.path_2d import Path2D
 
+# parameter for cuda device tests which skips test if cuda not available
+cuda_device_parameter = pytest.param("cuda", marks=pytest.mark.skipif(
+    not torch.cuda.is_available(),
+    reason="CUDA not available"
+))
+
 
 @pytest.mark.parametrize(
     "control_points, closed, yx_coords, expected",
@@ -22,31 +28,45 @@ from torch_subtract_membranes_2d.path_models.path_2d import Path2D
         (np.array([[0, 0], [0, 10], [10, 10], [10, 0]]), True, True, False),
     ]
 )
-def test_is_clockwise(control_points: np.ndarray, closed: bool, yx_coords: bool, expected: bool):
+def test_is_clockwise(
+        control_points: np.ndarray,
+        closed: bool,
+        yx_coords: bool,
+        expected: bool,
+):
+    control_points = torch.tensor(control_points, dtype=torch.float32)
     path = Path2D(control_points=control_points, is_closed=closed, yx_coords=yx_coords)
     assert path.is_clockwise is expected
 
 
-def test_open_path_interpolation():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_open_path_interpolation(device):
     # simple line from [0, 0] to [3, 0]
     control_points = np.array([[0, 0], [1, 0], [2, 0], [3, 0]])
+    control_points = torch.tensor(control_points, device=device, dtype=torch.float32)
     path = Path2D(control_points=control_points, is_closed=False)
 
-    u0 = torch.tensor([0]).float()
+    u0 = torch.tensor([0], device=device).float()
     interp_u0 = path.interpolate(u0)
-    assert torch.allclose(interp_u0, torch.tensor([0.0, 0.0]), atol=1e-5)
+    if device != "meta":
+        assert torch.allclose(interp_u0, torch.tensor([0.0, 0.0], device=device), atol=1e-5)
 
-    u0_5 = torch.tensor([0.5]).float()
+    u0_5 = torch.tensor([0.5], device=device).float()
     interp_u0_5 = path.interpolate(u0_5)
-    assert torch.allclose(interp_u0_5, torch.tensor([1.5, 0.0]), atol=1e-5)
+    if device != "meta":
+        assert torch.allclose(interp_u0_5, torch.tensor([1.5, 0.0], device=device), atol=1e-5)
 
-    u1 = torch.tensor([1]).float()
+    u1 = torch.tensor([1], device=device).float()
     interp_u1 = path.interpolate(u1)
-    assert torch.allclose(interp_u1, torch.tensor([3.0, 0.0]), atol=1e-5)
+    if device != "meta":
+        assert torch.allclose(interp_u1, torch.tensor([3.0, 0.0], device=device), atol=1e-5)
 
-def test_closed_path_interpolation():
+
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_closed_path_interpolation(device):
     # closed CW path with xy coordinates
     control_points = np.array([[0, 0], [0, 10], [10, 10], [10, 0]])
+    control_points = torch.tensor(control_points, device=device, dtype=torch.float32)
     path = Path2D(control_points=control_points, is_closed=True)
 
     # start point should be the same as end point
@@ -54,71 +74,89 @@ def test_closed_path_interpolation():
     u1 = torch.tensor([1]).float()
     interp_u0 = path.interpolate(u0)
     interp_u1 = path.interpolate(u1)
-    assert torch.allclose(interp_u0, interp_u1, atol=1e-5)
+    if device != "meta":
+        assert torch.allclose(interp_u0, interp_u1, atol=1e-5)
 
 
-def test_get_tangents():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_get_tangents(device):
     # simple line from [0, 0] to [3, 0]
     control_points = np.array([[0, 0], [1, 0], [2, 0], [3, 0]])
+    control_points = torch.tensor(control_points, device=device, dtype=torch.float32)
     path = Path2D(control_points=control_points, is_closed=False)
 
     u0 = torch.tensor([0]).float()
     tangent_u0 = path.get_tangents(u0)
-    assert torch.allclose(tangent_u0, torch.tensor([1.0, 0.0]))
+    if device != "meta":
+        assert torch.allclose(tangent_u0, torch.tensor([1.0, 0.0], device=device))
 
     u0_5 = torch.tensor([0.5]).float()
     tangent_u0_5 = path.get_tangents(u0_5)
-    assert torch.allclose(tangent_u0_5, torch.tensor([1.0, 0.0]))
+    if device != "meta":
+        assert torch.allclose(tangent_u0_5, torch.tensor([1.0, 0.0], device=device))
 
     u1 = torch.tensor([1]).float()
     tangent_u1 = path.get_tangents(u1)
-    assert torch.allclose(tangent_u1, torch.tensor([1.0, 0.0]))
+    if device != "meta":
+        assert torch.allclose(tangent_u1, torch.tensor([1.0, 0.0], device=device))
 
 
-def test_get_normals():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_get_normals(device):
     # simple line from [0, 0] to [3, 0]
     control_points = np.array([[0, 0], [1, 0], [2, 0], [3, 0]])
+    control_points = torch.tensor(control_points, device=device, dtype=torch.float32)
     path = Path2D(control_points=control_points, is_closed=False)
 
     u0 = torch.tensor([0]).float()
     normal_u0 = path.get_normals(u0)
-    assert torch.allclose(normal_u0, torch.tensor([0.0, -1.0]))
+    if device != "meta":
+        assert torch.allclose(normal_u0, torch.tensor([0.0, -1.0], device=device))
 
     u0_5 = torch.tensor([0.5]).float()
     normal_u0_5 = path.get_normals(u0_5)
-    assert torch.allclose(normal_u0_5, torch.tensor([0.0, -1.0]))
+    if device != "meta":
+        assert torch.allclose(normal_u0_5, torch.tensor([0.0, -1.0], device=device))
 
     u1 = torch.tensor([1]).float()
     normal_u1 = path.get_normals(u1)
-    assert torch.allclose(normal_u1, torch.tensor([0.0, -1.0]))
+    if device != "meta":
+        assert torch.allclose(normal_u1, torch.tensor([0.0, -1.0], device=device))
 
 
-def test_get_normals_yx():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_get_normals_yx(device):
     # simple line from [0, 0] to [3, 0]
     control_points = np.array([[0, 0], [0, 1], [0, 2], [0, 3]])
+    control_points = torch.tensor(control_points, device=device, dtype=torch.float32)
     path = Path2D(control_points=control_points, is_closed=False, yx_coords=True)
 
     u0 = torch.tensor([0]).float()
     normal_u0 = path.get_normals(u0)
-    assert torch.allclose(normal_u0, torch.tensor([-1.0, 0.0]))
+    if device != "meta":
+        assert torch.allclose(normal_u0, torch.tensor([-1.0, 0.0], device=device))
 
     u0_5 = torch.tensor([0.5]).float()
     normal_u0_5 = path.get_normals(u0_5)
-    assert torch.allclose(normal_u0_5, torch.tensor([-1.0, 0.0]))
+    if device != "meta":
+        assert torch.allclose(normal_u0_5, torch.tensor([-1.0, 0.0], device=device))
 
     u1 = torch.tensor([1]).float()
     normal_u1 = path.get_normals(u1)
-    assert torch.allclose(normal_u1, torch.tensor([-1.0, 0.0]))
+    if device != "meta":
+        assert torch.allclose(normal_u1, torch.tensor([-1.0, 0.0], device=device))
 
 
-def test_get_closest_u():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_get_closest_u(device):
     # simple line from [0, 0] to [3, 0]
-    control_points = torch.tensor([[0, 0], [0, 1], [0, 2], [0, 3]])
+    control_points = np.array([[0, 0], [0, 1], [0, 2], [0, 3]])
+    control_points = torch.tensor(control_points, device=device, dtype=torch.float32)
     path = Path2D(control_points=control_points, is_closed=False)
 
     # generate 100 points all 10 units away from the line
     xy = np.linspace(start=(10, 0), stop=(10, 3), num=100)
-    xy = torch.as_tensor(xy).float()
+    xy = torch.as_tensor(xy, device=device, dtype=torch.float32)
 
     # find closest u value and distance
     closest_u = path.get_closest_u(xy)
@@ -126,39 +164,48 @@ def test_get_closest_u():
     refined_distances = torch.linalg.norm(xy - closest_points, dim=-1)
 
     # check that refined distances match expected distances
-    expected_distances = torch.ones(size=(len(xy),)) * 10
-    assert torch.allclose(refined_distances, expected_distances, atol=1e-4)
+    expected_distances = torch.ones(size=(len(xy),), device=device) * 10
+    if device != "meta":
+        assert torch.allclose(refined_distances, expected_distances, atol=1e-4)
 
     # check that u values match expectations
-    expected_u = torch.linspace(0, 1, steps=100)
-    assert torch.allclose(closest_u, expected_u, atol=1e-3)
+    expected_u = torch.linspace(0, 1, steps=100, device=device)
+    if device != "meta":
+        assert torch.allclose(closest_u, expected_u, atol=1e-3)
 
 
-def test_as_uniformly_spaced_open_path():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_as_uniformly_spaced_open_path(device):
     # simple line from [0, 0] to [0, 4]
     control_points = np.array([[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]])
+    control_points = torch.tensor(control_points, device=device, dtype=torch.float32)
     path = Path2D(control_points=control_points, is_closed=False)
 
     new_path = path.as_uniformly_spaced(spacing=0.1)
     diffs = torch.diff(new_path.control_points, dim=0)
-    assert torch.allclose(diffs, torch.tensor([0.0, 0.1]), atol=1e-2)
+    if device != "meta":
+        assert torch.allclose(diffs, torch.tensor([0.0, 0.1], device=device), atol=1e-2)
 
 
-def test_as_uniformly_space_closed_path():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_as_uniformly_space_closed_path(device):
     # closed CW path with xy coordinates
     control_points = np.array([[0, 0], [0, 10], [10, 10], [10, 0]])
+    control_points = torch.tensor(control_points, device=device, dtype=torch.float32)
     path = Path2D(control_points=control_points, is_closed=True)
 
     new_path = path.as_uniformly_spaced(spacing=0.1)
     diffs = torch.diff(new_path.control_points, dim=0)
     lengths = torch.linalg.norm(diffs, dim=-1)
-    assert torch.allclose(lengths, torch.tensor([0.1]), atol=1e-2)
+    if device != "meta":
+        assert torch.allclose(lengths, torch.tensor([0.1], device=device), atol=1e-2)
 
 
-def test_optimization():
+@pytest.mark.parametrize("device", ["cpu", cuda_device_parameter])
+def test_optimization(device):
     # setup some initial 2d control points
-    initial_control_points = torch.zeros(size=(30, 2))
-    path = Path2D(control_points=initial_control_points, is_closed=False)
+    control_points = torch.zeros(size=(30, 2), device=device, dtype=torch.float32)
+    path = Path2D(control_points=control_points, is_closed=False)
     path.control_points.requires_grad_(True)
 
     # target function
@@ -174,7 +221,7 @@ def test_optimization():
         optimizer.zero_grad()
 
         # random samples
-        u = torch.rand(size=(100,))
+        u = torch.rand(size=(100,), device=device, dtype=torch.float32)
         ground_truth_x = u
         ground_truth_y = f(ground_truth_x)
 
